8472f14833576b7e892c8379d510dc510fa7d03f
Nwafor Chinemerem Divine <chinemeremdivine400@gmail.com>
2023-03-20 22:02:19 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 
-int n;  
+int n;
 
 srand(time(0));
 
@@ -19,7 +19,7 @@ if ((n % 10) > 5)
 
 {
 
-printf("Last digit of %d is %d and is greater than 5\n", n, n % 10); 
+printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 
 }
 
@@ -31,7 +31,7 @@ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 
 }
 
-else 
+else
 
 {
 
