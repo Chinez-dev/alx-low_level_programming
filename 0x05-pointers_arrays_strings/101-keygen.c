3490f7cae13ac0d5a7ae69e689cf3d5ac8eb90d2
Nwafor Chinemerem Divine <chinemeremdivine400@gmail.com>
2023-05-02 19:55:06 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,4 @@
-nclude <stdlib.h>
+#include <stdlib.h>
 
 #include <time.h>
 
@@ -24,7 +24,7 @@ for (i = 0; i < 100; i++)
 pass[i] = rand() % 78;
 sum += (pass[i] + '0');
 putchar(pass[i] + '0');
-if ((2772 - sum) - '0' < 78)
+if ((2772 - sum)-'0' < 78)
 {
 
 n = 2772 - sum - '0';
