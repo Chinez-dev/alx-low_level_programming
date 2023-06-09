c95dc0fb00430e24fa63b677b6147465b276be5a
Nwafor Chinemerem <chinemeremdivine400@gmail.com>
2023-06-09 11:56:07 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+*main- prints a program that counts the number
+*@argc: counts the number of argument
+*@argv: count the number of array
+*Return: Always 0
+*
+*
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
