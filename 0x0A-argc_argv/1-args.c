fea879f08677a5028baf2d3ac164cebaaf89b84a
Nwafor Chinemerem <chinemeremdivine400@gmail.com>
2023-06-09 17:06:58 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
-* main - print the number of arguments passed to the program
+/**
+* main- print the number of arguments passed to the program
 *@argc: counts the number of argument
 *@argv: count the number of array
 *Return: Always 0
@@ -10,6 +10,7 @@
 */
 
 int main(int argc, char *argv[])
+
 {
 	(void)argv;/*ignore argv*/
 	printf("%d\n", argc - 1);
